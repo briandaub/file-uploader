246d4e062e330c2b6f3677d15378a5cf3e26017b
Brian Daub <brian@rockridgecreative.com>
2016-06-22 00:04:48 -0700

    changed main.js
    Reading file line by line and adding it to an array. Then comparing the
    array elements against a regex to find sequence_id.

--- js/main.js
+++ js/main.js
@@ -15,19 +15,27 @@ function scanForSequences(event) {
     
     //If file has been uploaded
     if(file) {
+        var arrayOfLines = [];
+        var obj = {};
         //Create a new file reader
         var reader = new FileReader();
         //When the file reader loads
         reader.onload = function(evt) {
             //Add the contents of file to variable contents
-            var contents = evt.target.result;
+            var contentsByLine = evt.target.result.split('\n');
             //Alert user the file upload has succeeded
             alert('File ' + file.name + ' has been uploaded!');
             
             
-            for(var i = 0; i < contents.length; i++){
-
+            for(var i = 0; i < contentsByLine.length; i++){
+                arrayOfLines.push(contentsByLine);
+                console.log(arrayOfLines[i]);
+                if(arrayOfLines[i] == sequence_id){
+                    obj[i] = arrayOfLines[i];
+                    console.log(obj);
+                }
             }
+            
         }
         //Parse the file as text
         reader.readAsText(file);
