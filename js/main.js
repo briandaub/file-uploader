4fe57b677223a931db770ff589f74074f71fa7f4
briandaub <brian@rockridgecreative.com>
2016-06-30 11:18:38 -0700

    added template

--- js/main.js
+++ js/main.js
@@ -35,9 +35,38 @@ function parse(event) {
                 }else{
                     obj.sequenceArray.push(contentsByLine[i]);
                 }
-               // console.log(objArray[i]['sequence']);
             }
-            console.log(objArray)
+            
+            
+            // John Resig - http://ejohn.org/ - MIT Licensed
+            (function () {
+                var cache = {};
+                this.tmpl = function tmpl(str, data) {
+                  // Figure out if we're getting a template, or if we need to
+                  // load the template - and be sure to cache the result.
+                  // RegExp - Match any non word character in a str ('%' in '50%')
+                  var fn = !/\W/.test(str) ?
+                      cache[str] = cache[str] ||
+                      tmpl(document.getElementById(str).innerHTML) :
+                    // Generate a reusable function that will serve as a template
+                    // generator (and which will be cached).
+                    new Function("obj",
+                        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+                        // Introduce the data as local variables using with(){}
+                        "with(obj){p.push('" +
+                        // Convert the template into pure JavaScript
+                          str
+                              .replace(/[\r\t\n]/g, " ")
+                              .split("<%").join("\t")
+                              .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+                              .replace(/\t=(.*?)%>/g, "',$1,'")
+                              .split("\t").join("');")
+                              .split("%>").join("p.push('")
+                              .split("\r").join("\\'") + "');}return p.join('');");
+                  // Provide some basic currying to the user
+                  return data ? fn(data) : fn;
+                };
+            })();
 
             // Create the DataView.
             var dataView = new Slick.Data.DataView();
@@ -60,7 +89,7 @@ function parse(event) {
             var result;
             
             for (var i = 0; i < objArray.length; i++) {
-                objArray[i]['sequence'] = objArray[i]['id'] + '\n' + objArray[i]['sequenceArray'].join('');
+                objArray[i]['sequence'] = objArray[i]['id'] + '\n' + objArray[i]['sequenceArray'].join('\n');
                 data.push(objArray[i]);
             }
 
